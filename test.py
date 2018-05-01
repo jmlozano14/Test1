27c72597d4b2d28f3bf5fad8c3408a5f26f6c46a
Jorge Mario Lozano Ramirez <jmlozano@outlook.con>
2018-05-01 17:33:01 -0500

    final print

--- test.py
+++ test.py
@@ -1,8 +1,3 @@
 # python test in github
 c = 2
-<<<<<<< HEAD
 print "This is the master branch, no one has more power than me"
-=======
-print 'Try to make a pull request'
-print 'It sound so usefull for my codes'
->>>>>>> 77f1b7571561b1d606d40edcc3f2ed017776bc92
